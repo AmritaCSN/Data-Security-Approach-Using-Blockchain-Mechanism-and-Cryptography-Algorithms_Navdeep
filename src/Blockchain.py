74e51eadd2df4911a173426ee89179040c17033a
Navdeep1999 <49747099+Navdeep1999@users.noreply.github.com>
2023-05-25 22:53:56 +0530

    Update Blockchain.py

--- src/Blockchain.py
+++ src/Blockchain.py
@@ -1,11 +1,6 @@
 from Block import Block
 
-'''
-Remember block structure :
-    (previous_hash, data)
-where 
-    data is the encrypted data using one of the algo : AES, DES, or RC6
-'''
+
 class Blockchain():
     def __init__(self):
             block_0 = self.create_genesis_block()
@@ -17,10 +12,7 @@ class Blockchain():
         return genesis_block
 
     def create_new_block(self, encrypted_text):
-        '''
-        this will be called for storing new data - of a bigger length
-        hardcoding the message and key
-        '''
+    
         previous_block = self.chain[-1]
         new_block = Block(previous_block.hash, encrypted_text)
         return new_block
